528c227b96ed0afe313d00d77d7081ef3f956175
Dipankaj Singh <dipankajsingh2@gmail.com>
2023-04-10 23:53:58 +0530

    update

--- index.js
+++ index.js
@@ -1,41 +1,14 @@
-// function onScanSuccess(decodedText, decodedResult) {
-//     // handle the scanned code as you like, for example:
-//     console.log(`Code matched = ${decodedText}`, decodedResult);
-// }
-
-// function onScanFailure(error) {
-//     // handle scan failure, usually better to ignore and keep scanning.
-//     // for example:
-//     console.warn(`Code scan error = ${error}`);
-// }
-
-// let html5QrcodeScanner = new Html5QrcodeScanner(
-//     "reader",
-//     { fps: 10, qrbox: { width: 250, height: 250 } },
-//     /* verbose= */ false);
-// html5QrcodeScanner.render(onScanSuccess, onScanFailure);
-
-
-
-
-// This method will trigger user permissions
-// Html5Qrcode.getCameras().then(devices => {
-//     /**
-//      * devices would be an array of objects of type:
-//      * { id: "id", label: "label" }
-//      */
-//     if (devices && devices.length) {
-//       var cameraId = devices[0].id;
-//       // .. use this to start scanning.
-//     }
-//   }).catch(err => {
-//     // handle err
-//   });
 let scanCountElm = document.querySelector(".scanCount")
 const video = document.querySelector(".camera")
 let isScannig = false
 const scanControl = document.querySelector('.scanControl')
 let currentStream = null
+
+let scanner = new Html5Qrcode('scanner');
+
+const config = { fps: 10 };
+
+
 
 const constraints = {
     audio: false,
@@ -55,7 +28,9 @@ const constraints = {
 };
 
 scanControl.addEventListener("click", () => {
-
+    scanner.start({ facingMode: "environment" }, config, (text, result) => {
+        console.log(text, result)
+    });
     scanCountElm.innerHTML = navigator.mediaDevices
 
     isScannig = !isScannig
@@ -75,7 +50,11 @@ scanControl.addEventListener("click", () => {
     }
     if (!isScannig) {
         let track = currentStream.getTracks()[0]
-
+        scanner.stop().then((ignore) => {
+            console.log(ignore)
+        }).catch((err) => {
+            console.log(err)
+        });
         if (track.readyState == 'live' && track.kind === 'video') {
             track.stop();
         }
