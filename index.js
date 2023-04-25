9fa6b2cbf278bf40cf977c3206ce17ac40ece6c9
Dipankaj Singh <dipankajsingh2@gmail.com>
2023-04-25 08:52:43 +0530

    a lot done

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+const data = []
+
 function onScanSuccess(decodedText, decodedResult) {
     document.getElementsByTagName('p')[0].innerText += decodedResult + "  " + decodedText
+    if (data.includes(decodedResult)) return
+    data.push(decodedResult)
 }
 var html5QrcodeScanner = new Html5QrcodeScanner(
     "qr-reader", { fps: 10, qrbox: 250 });
 html5QrcodeScanner.render(onScanSuccess);
+
+
+function downloadCSV() {
+    csv = 'data:text/csv;charset=utf-8,' + data.toString(); //Creates CSV File Format
+    excel = encodeURI(csv); //Links to CSV 
+
+    link = document.createElement('a');
+    link.setAttribute('href', excel); //Links to CSV File 
+    link.setAttribute('download', 'test.csv'); //Filename that CSV is saved as
+    link.click();
+}
